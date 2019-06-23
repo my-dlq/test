1cf0ed0ce45994f51c494688e218798b0d8720ba
dingliqiang <dingliqiang@aspirecn.com>
2019-06-23 13:38:09 +0800

    测试

--- src/main/java/club/mydlq/admin/Application.java
+++ src/main/java/club/mydlq/admin/Application.java
@@ -1,9 +1,13 @@
-package club.mydlq.springboothelloword;
+package club.mydlq.admin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableAdminServer
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
