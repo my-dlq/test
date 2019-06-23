8362dcee883db1c1c3eab95cc783e7dd0473faa8
dingliqiang <dingliqiang@aspirecn.com>
2019-06-23 22:31:25 +0800

    对对对

--- src/main/java/club/mydlq/admin/Application.java
+++ src/main/java/club/mydlq/admin/Application.java
@@ -4,13 +4,11 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableAdminServer
-@EnableAsync
 @EnableDiscoveryClient
+@EnableAdminServer
 @EnableScheduling
 public class Application {
 
